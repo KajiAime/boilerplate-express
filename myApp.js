95ff1a50ad3127221eb51d5ff5f67687db4a6e0c
KajiAime <ndekaji@gmail.com>
2023-05-22 00:57:55 +0100

    I first displayed a HTML file and caused the CSS file in the 'public' folder to display too. I then added a new path for my API to show some json on screen.

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/json', function(req, res) {
+    res.json({"message": "Hello json"});
+})
+
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
