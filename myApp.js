6fcf4489b8665fe624d7d6d40ee78d5d0635cfd8
KajiAime <ndekaji@gmail.com>
2023-05-24 22:54:58 +0100

    I just made use of the .env file to capitalize a phrase. Doing this on localhost is not easy.

--- myApp.js
+++ myApp.js
@@ -1,12 +1,20 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
+
 app.get('/json', function(req, res) {
-    res.json({"message": "Hello json"});
-})
+    let texts = "Hello json";
+    const cases = process.env.MESSAGE_STYLE;
+    if(cases === "uppercase"){
+        res.json({message: texts.toUpperCase()});
+    }else{
+        res.json({message:texts});
+    }
+});
 
 app.use('/public', express.static(__dirname + '/public'));
 
