0eaf9cae39d33d2960eef991e74c83a539f670fe
KajiAime <ndekaji@gmail.com>
2023-05-24 23:48:57 +0100

    I chained middlewares on the /now route in the myApp.js file. It only had to display the current date and type.

--- myApp.js
+++ myApp.js
@@ -7,6 +7,14 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res){
+    res.send({time: req.time});
+}
+);
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
